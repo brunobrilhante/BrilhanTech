6fa9d56e8728289e2edc3537e7ac674ae2fbce4a
Bruno Brilhante <brunobrilhante53@gmail.com>
2022-04-16 19:24:45 -0300

    Adicionando as pastas de front e de back

---
+++ backend/BrilhanTech/src/main/java/com/brilhantech/BrilhanTechApplication.java
@@ -0,0 +1,13 @@
+package com.brilhantech;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BrilhanTechApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BrilhanTechApplication.class, args);
+	}
+
+}
---
+++ backend/BrilhanTech/src/test/java/com/brilhantech/BrilhanTechApplicationTests.java
@@ -0,0 +1,13 @@
+package com.brilhantech;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BrilhanTechApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
