fb370253a98a279528ed9c4ef79d32eaf1b4f778
Bruno Brilhante <brunobrilhante53@gmail.com>
2022-04-23 16:14:59 -0300

    Criando processo de instanciação e classe Produto

--- backend/BrilhanTech/src/main/java/com/brilhantech/BrilhanTechApplication.java
+++ backend/BrilhanTech/src/main/java/com/brilhantech/BrilhanTechApplication.java
@@ -1,13 +1,34 @@
 package com.brilhantech;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class BrilhanTechApplication {
+import com.brilhantech.domain.Categoria;
+import com.brilhantech.repositories.CategoriaRepository;
 
+@SpringBootApplication
+public class BrilhanTechApplication implements CommandLineRunner {
+
+	@Autowired
+	private CategoriaRepository categoriaRepository;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(BrilhanTechApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {			
+		
+		Categoria cat1 = new Categoria(null, "Gabinete");
+		Categoria cat2 = new Categoria(null, "Monitor");
+		Categoria cat3 = new Categoria(null, "Mouse");
+		
+		categoriaRepository.saveAll(Arrays.asList(cat1, cat2, cat3));
+		
 	}
 
 }
---
+++ backend/BrilhanTech/src/main/java/com/brilhantech/domain/Produto.java
@@ -0,0 +1,5 @@
+package com.brilhantech.domain;
+
+public class Produto {
+
+}
